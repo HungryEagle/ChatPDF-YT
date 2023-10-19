3a7a264b52306233ccf419d3d1dd4b20340f121f
venugopalan_iyengar <venugopalan@everwell.org>
2023-10-19 18:28:20 +0530

    Added chat side bar and chat window

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,18 @@ import { Button } from '@/components/ui/button'
 import { UserButton } from '@clerk/nextjs/app-beta'
 import { auth } from '@clerk/nextjs/server'
 import Link from 'next/link';
-import {LogIn} from 'lucide-react'
+import { LogIn } from 'lucide-react'
 
 export default async function Home() {
-  const {userId} = await auth()
+  const { userId } = await auth()
   const isAuth = !!userId;
-  return(
+  return (
     <div className='m-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100'>
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <div className="flex flex-col items-center text-center">
           <div className="flex items-center">
-            <h1 className='mr-3 text-5xl font-semibold'>Chat with any PDF</h1>
-            <UserButton afterSignOutUrl="/"/>
+            <h1 className='mr-3 text-5xl font-semibold'>Chat with the TB Assistant</h1>
+            <UserButton afterSignOutUrl="/" />
           </div>
 
           <div className="flex mt-2">
@@ -21,14 +21,14 @@ export default async function Home() {
           </div>
 
           <p className='max-w-xl mt-1 text-lg text-slate-600'>
-            Join millions of students, researchers and professionals to instantly answer questions with AI
+            Instantly Connect with Tuberculosis Guidelines at Your Fingertips!
           </p>
 
           <div className="w-full mt-4">
-            {isAuth ? (<h1>File Upload</h1>):(
+            {isAuth ? (<h1>File Upload</h1>) : (
               <Link href="/sign-in">
                 <Button>Login to get started
-                  <LogIn className='w-4 h-4 ml-2'/>
+                  <LogIn className='w-4 h-4 ml-2' />
                 </Button>
               </Link>
             )}
